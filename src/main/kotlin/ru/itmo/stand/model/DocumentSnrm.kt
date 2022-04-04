65123ef51ed2d237835b774817ec24fd666b7e4a
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2022-04-05 00:15:21 +0400

    Field type is specified

--- src/main/kotlin/ru/itmo/stand/model/DocumentSnrm.kt
+++ src/main/kotlin/ru/itmo/stand/model/DocumentSnrm.kt
@@ -3,6 +3,7 @@ package ru.itmo.stand.model
 import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.Document
 import org.springframework.data.elasticsearch.annotations.Field
+import org.springframework.data.elasticsearch.annotations.FieldType
 import ru.itmo.stand.model.DocumentSnrm.Companion.DOCUMENT_SNRM
 
 @Document(indexName = DOCUMENT_SNRM)
@@ -10,7 +11,7 @@ data class DocumentSnrm(
     @Id
     var id: String? = null,
 
-    @Field(index = false)
+    @Field(index = false, type = FieldType.Text)
     val content: String,
 
     @Field
