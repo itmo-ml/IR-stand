66a7a93f5fb8d63bba23505b230307fdc716fe30
abramovich <abramovich@speechpro.com>
2022-04-24 20:11:08 +0300

    repos for tokens, docs and counter. persist redis

--- src/main/kotlin/ru/itmo/stand/config/StandProperties.kt
+++ src/main/kotlin/ru/itmo/stand/config/StandProperties.kt
@@ -7,8 +7,13 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConfigurationProperties(value = "stand")
 data class StandProperties(
     val elasticsearch: ElasticsearchProperties,
+    val redis: RedisProperties
 ) {
     data class ElasticsearchProperties(
         val hostAndPort: String,
+    )
+    data class RedisProperties(
+        val host: String,
+        val port: Int
     )
 }
