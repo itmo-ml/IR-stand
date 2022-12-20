2c1a9a1869fa81b2b1bc327e0c75701622aff04b
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2022-12-20 23:08:31 +0400

    feat: add TypeAlias

--- src/main/kotlin/ru/itmo/stand/storage/mongodb/model/neighbours/ContextualizedVector.kt
+++ src/main/kotlin/ru/itmo/stand/storage/mongodb/model/neighbours/ContextualizedVector.kt
@@ -1,10 +1,12 @@
 package ru.itmo.stand.storage.mongodb.model.neighbours
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.TypeAlias
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
+@TypeAlias("ContextualizedVector")
 data class ContextualizedVector(
     @Id
     val id: String? = null,
