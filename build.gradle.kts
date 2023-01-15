a0f26a1769c630455ca9dff79dca856e52d8697d
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2023-01-15 21:20:50 +0400

    test: add mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:${findProperty("mockkVersion")}")
 }
 
 tasks.withType<KotlinCompile> {
