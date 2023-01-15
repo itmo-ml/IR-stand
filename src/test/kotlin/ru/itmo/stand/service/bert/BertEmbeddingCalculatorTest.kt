9c90a1b93a4f5ed3d79f1777bc4cf081d15f46a9
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2023-01-15 20:53:07 +0400

    test: return the same result as for Python

--- src/test/kotlin/ru/itmo/stand/service/bert/BertEmbeddingCalculatorTest.kt
+++ src/test/kotlin/ru/itmo/stand/service/bert/BertEmbeddingCalculatorTest.kt
@@ -11,16 +11,27 @@ class BertEmbeddingCalculatorTest {
     )
 
     @Test
+    fun `should return the same result as for Python`() {
+        val content = "test 1"
+
+        val embedding = embeddingCalculator.calculate(content)
+
+        assertThat(embedding)
+            .usingComparatorWithPrecision(0.000_001f)
+            .startsWith(0.216_166f, -0.093_901f, -0.137_327f, 0.097_924f, 0.406_257f)
+    }
+
+    @Test
     fun `should return same result for batch mode`() {
         val content1 = "test 1"
         val content2 = "test 2 3"
         val content3 = "test 3 4 5"
-        val vector1 = embeddingCalculator.calculate(content1)
-        val vector2 = embeddingCalculator.calculate(content2)
-        val vector3 = embeddingCalculator.calculate(content3)
+        val embedding1 = embeddingCalculator.calculate(content1)
+        val embedding2 = embeddingCalculator.calculate(content2)
+        val embedding3 = embeddingCalculator.calculate(content3)
 
         val embeddingsBatch = embeddingCalculator.calculate(arrayOf(content1, content2, content3))
 
-        assertThat(embeddingsBatch).isEqualTo(arrayOf(vector1, vector2, vector3))
+        assertThat(embeddingsBatch).isEqualTo(arrayOf(embedding1, embedding2, embedding3))
     }
 }
