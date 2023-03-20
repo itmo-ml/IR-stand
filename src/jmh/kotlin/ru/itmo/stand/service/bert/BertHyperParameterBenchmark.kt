93ead6b398dc88b7ecf08189a0aa221f83e5e0aa
roman <asmetliness24237@gmail.com>
2023-03-20 21:43:47 +0300

    update benchmark

--- src/jmh/kotlin/ru/itmo/stand/service/bert/BertHyperParameterBenchmark.kt
+++ src/jmh/kotlin/ru/itmo/stand/service/bert/BertHyperParameterBenchmark.kt
@@ -2,13 +2,19 @@ package ru.itmo.stand.service.bert
 
 import ai.djl.huggingface.translator.TextEmbeddingTranslatorFactory
 import ai.djl.repository.zoo.Criteria
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.BenchmarkMode
 import org.openjdk.jmh.annotations.Mode
 import org.openjdk.jmh.annotations.Scope
 import org.openjdk.jmh.annotations.State
-import org.openjdk.jmh.annotations.Benchmark
 import ru.itmo.stand.fixtures.preprocessingPipelineExecutor
 import java.nio.file.Paths
+import java.util.concurrent.atomic.AtomicInteger
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
@@ -36,13 +42,16 @@ open class BertHyperParameterBenchmark {
             .optArgument("padding", "true")
             .optArgument("normalize", "false")
             .optArgument("pooling", "cls")
+            .optArgument("maxLength", "20")
             .optTranslatorFactory(TextEmbeddingTranslatorFactory())
             .build()
             .loadModel()
-            .newPredictor()
+
+    private val predictor = tinyModel.newPredictor()
 
     private val testContents = generateWindows()
 
+/*
     @Benchmark
     fun singleThreadBenchmark_100(): Array<FloatArray> {
         return singleThreadBenchmark(100)
@@ -67,12 +76,17 @@ open class BertHyperParameterBenchmark {
     fun singleThreadBenchmark_2000(): Array<FloatArray> {
         return singleThreadBenchmark(2000)
     }
+*/
 
-    @Benchmark
-    fun singleThreadBenchmark_5000(): Array<FloatArray> {
-        return singleThreadBenchmark(5000)
-    }
+//    @Benchmark
+//    fun singleThreadBenchmark_5000(): Array<FloatArray> {
+//        return singleThreadBenchmark(5000)
+//    }
 
+
+
+
+/*
     @Benchmark
     fun singleThreadBenchmark_10_000(): Array<FloatArray> {
         return singleThreadBenchmark(10_000)
@@ -96,13 +110,45 @@ open class BertHyperParameterBenchmark {
     @Benchmark
     fun singleThreadBenchmark_50_000(): Array<FloatArray> {
         return singleThreadBenchmark(50_000)
+    }*/
+
+
+    @Benchmark
+    fun multithreadedBenchmark_4_5000(): Array<FloatArray> {
+        return multithreadedBenchmark(5000, 4)
     }
 
     private fun singleThreadBenchmark(batchSize: Int): Array<FloatArray> {
         return testContents.chunked(batchSize)
             .flatMap {
-                tinyModel.predict(it.toTypedArray()).asIterable()
+                predictor.predict(it.toTypedArray()).asIterable()
             }.toTypedArray()
+    }
+
+
+
+    private fun multithreadedBenchmark(batchSize: Int, numThreads: Int): Array<FloatArray> = runBlocking(Dispatchers.Default) {
+
+        val counter = AtomicInteger(0)
+        val chan = Channel<List<String>>(numThreads)
+        repeat(numThreads) {
+            launch {
+                val predictor = tinyModel.newPredictor()
+                for(data in chan) {
+                    counter.incrementAndGet()
+                    predictor.predict(data.toTypedArray())
+                }
+                predictor.close()
+            }
+        }
+
+        for(data in testContents.chunked(batchSize)) {
+            chan.send(data)
+        }
+        while(!chan.isEmpty) {}
+        chan.close()
+
+        arrayOf()
     }
 
     private fun generateWindows(count: Int = 50_000): List<String> {
