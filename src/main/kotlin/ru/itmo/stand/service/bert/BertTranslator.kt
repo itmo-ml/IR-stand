cd487b2f0892f360687fc6c95c0e21481c823e78
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2023-03-27 21:19:22 +0400

    refactor: extract vocabulary creation to bertVocabulary val

--- src/main/kotlin/ru/itmo/stand/service/bert/BertTranslator.kt
+++ src/main/kotlin/ru/itmo/stand/service/bert/BertTranslator.kt
@@ -1,13 +1,11 @@
 package ru.itmo.stand.service.bert
 
-import ai.djl.modality.nlp.DefaultVocabulary
 import ai.djl.modality.nlp.Vocabulary
 import ai.djl.ndarray.NDList
 import ai.djl.translate.Translator
 import ai.djl.translate.TranslatorContext
 import org.springframework.stereotype.Service
-import ru.itmo.stand.util.UNKNOWN_TOKEN
-import ru.itmo.stand.util.getResource
+import ru.itmo.stand.util.bertVocabulary
 import ru.itmo.stand.util.wrapToClsAndSep
 
 @Service
@@ -16,11 +14,7 @@ class BertTranslator : Translator<List<String>, FloatArray> {
     private lateinit var vocabulary: Vocabulary
 
     override fun prepare(ctx: TranslatorContext) {
-        vocabulary = DefaultVocabulary.builder()
-            .optMinFrequency(1)
-            .addFromTextFile(getResource("./data/bert/vocab.txt"))
-            .optUnknownToken(UNKNOWN_TOKEN)
-            .build()
+        vocabulary = bertVocabulary
     }
 
     override fun processInput(ctx: TranslatorContext, input: List<String>): NDList {
