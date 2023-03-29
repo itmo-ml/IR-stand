b27b6b78379e7debce54cffdaf6ecba9331c0d8c
abramovich <abramovich@speechpro.com>
2023-03-29 10:21:10 +0300

    update

--- src/main/kotlin/ru/itmo/stand/StandApplication.kt
+++ src/main/kotlin/ru/itmo/stand/StandApplication.kt
@@ -1,10 +1,12 @@
 package ru.itmo.stand
 
 import CustomEmbeddingTranslatorFactory
+import ai.djl.huggingface.tokenizers.HuggingFaceTokenizer
 import ai.djl.repository.zoo.Criteria
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import ru.itmo.stand.config.StandProperties
+import ru.itmo.stand.service.bert.CustomTranslatorInput
 import java.nio.file.Paths
 
 @SpringBootApplication
@@ -13,7 +15,7 @@ class StandApplication
 
 fun main(args: Array<String>) {
     val tinyModel = Criteria.builder()
-        .setTypes(Array<String>::class.java, Array<FloatArray>::class.java)
+        .setTypes(Array<CustomTranslatorInput>::class.java, Array<FloatArray>::class.java)
         .optModelName("prajjwal1/bert-tiny")
         .optModelPath(Paths.get("./models/bert-tiny"))
         .optEngine("PyTorch")
@@ -24,6 +26,16 @@ fun main(args: Array<String>) {
         .optTranslatorFactory(CustomEmbeddingTranslatorFactory())
         .build()
         .loadModel()
+
+    val tokenizer = HuggingFaceTokenizer.newInstance(Paths.get("./models/bert-tiny/tokenizer.json"))
+
+    val text = "Some input text for testing"
+    val tokens = tokenizer.tokenize(text)
+
+    val window = tokens.take(5).joinToString(" ")
+
+    val predictor = tinyModel.newPredictor()
+    val prediction = predictor.predict(arrayOf(CustomTranslatorInput(2, window)))
 
 //    exitProcess(SpringApplication.exit(runApplication<StandApplication>(*args)))
 }
