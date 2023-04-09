d7d0da60456d83fda2d756692ae725f638b26a4b
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2023-04-09 11:33:38 +0400

    feat: build inverted index

--- src/main/kotlin/ru/itmo/stand/service/impl/neighbours/indexing/InvertedIndexBuilder.kt
+++ src/main/kotlin/ru/itmo/stand/service/impl/neighbours/indexing/InvertedIndexBuilder.kt
@@ -3,40 +3,63 @@ package ru.itmo.stand.service.impl.neighbours.indexing
 import org.springframework.stereotype.Service
 import ru.itmo.stand.service.bert.BertEmbeddingCalculator
 import ru.itmo.stand.storage.embedding.EmbeddingStorageClient
+import ru.itmo.stand.storage.embedding.model.ContextualizedEmbedding
+import ru.itmo.stand.storage.lucene.model.NeighboursDocument
+import ru.itmo.stand.storage.lucene.repository.NeighboursDocumentRepository
+import ru.itmo.stand.storage.lucene.repository.NeighboursEmbeddingRepository
+import ru.itmo.stand.util.dot
 import java.io.File
 
 @Service
 class InvertedIndexBuilder(
+    private val neighboursEmbeddingRepository: NeighboursEmbeddingRepository,
+    private val neighboursDocumentRepository: NeighboursDocumentRepository,
     private val embeddingStorageClient: EmbeddingStorageClient,
     private val embeddingCalculator: BertEmbeddingCalculator,
 ) {
     fun index(windowedTokensFile: File) {
-        val tokens = windowedTokensFile
-            .bufferedReader()
-            .lineSequence()
-            .map { line ->
-                val (token, windowsString) = line.split(WindowedTokenCreator.TOKEN_WINDOWS_SEPARATOR)
-                val docIdsByWindowPairs = windowsString
-                    .split(WindowedTokenCreator.WINDOWS_SEPARATOR)
-                    .filter { it.isNotBlank() }
-                    .map {
-                        val (window, docIdsString) = it.split(WindowedTokenCreator.WINDOW_DOC_IDS_SEPARATOR)
-                        val docIds = docIdsString.split(WindowedTokenCreator.DOC_IDS_SEPARATOR)
-                        window to docIds
-                    }
-                TokenWithWindows(token, docIdsByWindowPairs)
-            }
+        val tokensWithWindows = readTokensWithWindows(windowedTokensFile)
 
-        tokens.forEach { tokenWithWindows ->
-            val (token, docIdsByWindowPairs) = tokenWithWindows
+        tokensWithWindows.forEach { tokenWithWindows ->
+            val (_, docIdsByWindowPairs) = tokenWithWindows
             val (windows, docIdsList) = docIdsByWindowPairs.unzip()
-            val embeddings = embeddingCalculator.calculate(windows.toTypedArray())
-            embeddings.forEach { embedding ->
-                embeddingStorageClient.findByVector(embedding.toTypedArray()).forEach {
-                    TODO("Not yet implemented")
-                }
+            embeddingCalculator.calculate(windows.toTypedArray()).forEachIndexed { index, embedding ->
+                val docIds = docIdsList[index]
+                embeddingStorageClient.findByVector(embedding.toTypedArray())
+                    .forEach { computeScoreAndSave(docIds, it) }
             }
         }
+    }
+
+    private fun readTokensWithWindows(windowedTokensFile: File) = windowedTokensFile
+        .bufferedReader()
+        .lineSequence()
+        .map { line ->
+            val (token, windowsString) = line.split(WindowedTokenCreator.TOKEN_WINDOWS_SEPARATOR)
+            val docIdsByWindowPairs = windowsString
+                .split(WindowedTokenCreator.WINDOWS_SEPARATOR)
+                .filter { it.isNotBlank() }
+                .map {
+                    val (window, docIdsString) = it.split(WindowedTokenCreator.WINDOW_DOC_IDS_SEPARATOR)
+                    val docIds = docIdsString.split(WindowedTokenCreator.DOC_IDS_SEPARATOR)
+                    window to docIds
+                }
+            TokenWithWindows(token, docIdsByWindowPairs)
+        }
+
+    private fun computeScoreAndSave(
+        docIds: List<String>,
+        contextualizedEmbedding: ContextualizedEmbedding,
+    ) {
+        val neighboursDocuments = docIds.map { docId ->
+            val documentEmbedding = neighboursEmbeddingRepository.findByDocId(docId).embedding // TODO: cache?
+            NeighboursDocument(
+                tokenWithEmbeddingId = "${contextualizedEmbedding.token}:${contextualizedEmbedding.embeddingId}",
+                docId = docId,
+                score = documentEmbedding.dot(contextualizedEmbedding.embedding.toFloatArray()),
+            )
+        }
+        neighboursDocumentRepository.saveAll(neighboursDocuments)
     }
 
     data class TokenWithWindows(
