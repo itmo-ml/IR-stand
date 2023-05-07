59f91a03c2f0acfd70584a4734244b51a643f8ee
abramovich <abramovich@speechpro.com>
2023-05-07 13:16:46 +0300

    fix format

--- src/main/kotlin/ru/itmo/stand/service/impl/neighbours/PreprocessingPipelineExecutor.kt
+++ src/main/kotlin/ru/itmo/stand/service/impl/neighbours/PreprocessingPipelineExecutor.kt
@@ -7,17 +7,16 @@ import ru.itmo.stand.service.preprocessing.StopWordRemover
 import ru.itmo.stand.service.preprocessing.TextCleaner
 import ru.itmo.stand.service.preprocessing.Tokenizer
 import ru.itmo.stand.util.Window
-import ru.itmo.stand.util.bertTokenizer
 
 @Service
 class PreprocessingPipelineExecutor(
-        private val standProperties: StandProperties,
-        private val contextSplitter: ContextSplitter,
-        private val stopWordRemover: StopWordRemover,
-        private val textCleaner: TextCleaner,
-        private val tokenizer: Tokenizer,
+    private val standProperties: StandProperties,
+    private val contextSplitter: ContextSplitter,
+    private val stopWordRemover: StopWordRemover,
+    private val textCleaner: TextCleaner,
+    private val tokenizer: Tokenizer,
 
-        ) {
+) {
 
     fun execute(content: String): List<Window> {
         val cleanedContent = textCleaner.preprocess(content)
