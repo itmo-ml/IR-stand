505821229b8d8d99d4c53ee2fc7ec5d299968a5b
abramovich <abramovich@speechpro.com>
2023-05-10 23:17:43 +0300

    upd

--- src/main/kotlin/ru/itmo/stand/service/impl/neighbours/PreprocessingPipelineExecutor.kt
+++ src/main/kotlin/ru/itmo/stand/service/impl/neighbours/PreprocessingPipelineExecutor.kt
@@ -5,8 +5,8 @@ import ru.itmo.stand.config.StandProperties
 import ru.itmo.stand.service.preprocessing.ContextSplitter
 import ru.itmo.stand.service.preprocessing.StopWordRemover
 import ru.itmo.stand.service.preprocessing.TextCleaner
+import ru.itmo.stand.service.preprocessing.Tokenizer
 import ru.itmo.stand.util.Window
-import ru.itmo.stand.util.bertTokenizer
 
 @Service
 class PreprocessingPipelineExecutor(
@@ -14,11 +14,12 @@ class PreprocessingPipelineExecutor(
     private val contextSplitter: ContextSplitter,
     private val stopWordRemover: StopWordRemover,
     private val textCleaner: TextCleaner,
+    private val tokenizer: Tokenizer
 ) {
 
     fun execute(content: String): List<Window> {
         val cleanedContent = textCleaner.preprocess(content)
-        val tokens = bertTokenizer.tokenize(cleanedContent)
+        val tokens = tokenizer.preprocess(cleanedContent)
         val tokensWithoutStopWords = stopWordRemover.preprocess(tokens) // TODO: configure this value (try to delete it?)
         val windowSize = standProperties.app.neighboursAlgorithm.tokenBatchSize
         return contextSplitter.preprocess(ContextSplitter.Input(tokensWithoutStopWords, windowSize))
