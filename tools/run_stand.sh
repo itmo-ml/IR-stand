b2beb643650551c60d0bc03719bbd2d2eb2fdd4a
Vyacheslav Dobrynin <w.v.dobr@gmail.com>
2023-08-09 16:47:28 +0400

    feat: add run_stand.sh script

---
+++ tools/run_stand.sh
@@ -0,0 +1,31 @@
+# Initialize
+method=bm25
+base_path=/your/base/path/
+collection_path=collections/collection.500k.tsv
+queries_path=collections/queries.500k.tsv
+qrels_path=collections/qrels.500k.tsv
+
+# Rebuild
+cd $base_path || { echo "Failure"; exit 1; }
+$base_path/gradlew clean bootJar
+
+# Remove previous
+rm -rf $base_path/indexes/$method/
+rm -rf $base_path/outputs/$method/
+
+# Index
+java -jar \
+  -Dstand.app.method=$method \
+  -Dstand.app.base-path=$base_path \
+  $base_path/build/libs/stand-0.0.1-SNAPSHOT.jar \
+  save-in-batch --with-id $collection_path
+
+# Search
+java -jar \
+  -Dstand.app.method=$method \
+  -Dstand.app.base-path=$base_path \
+  $base_path/build/libs/stand-0.0.1-SNAPSHOT.jar \
+  search -f MS_MARCO $queries_path
+
+# Get MRR
+python3 tools/msmarco_eval.py $qrels_path outputs/$method/resultInMrrFormat.tsv
